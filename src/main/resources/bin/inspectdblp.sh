2a1edd59aa79cb4d675dc5fddfc20f3ce71391af
Ashesh Kumar Singh <asing80@uic.edu>
2019-10-22 22:23:51 -0500

    Update inspection script

--- src/main/resources/bin/inspectdblp.sh
+++ src/main/resources/bin/inspectdblp.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 grep -o '</article>\|</inproceedings>\|</proceedings>\|</book>\|</incollection>\|</phdthesis>\|</mastersthesis>\|</www>' ../dblp.xml | wc -l
+
+
+grep -o '</author>' dblp.xml | wc -l # use multi
+grep -o '</journal>' dblp.xml | wc -l
+grep -o '</school>' dblp.xml | wc -l # use multi
+grep -o '</year>' dblp.xml | wc -l
+grep -o '</crossref>' dblp.xml| wc -l
+grep -o '</ee>' dblp.xml| wc -l #use multi
