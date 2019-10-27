e61c414ebc58b73352d69b44318647b3292687b7
Ashesh Kumar Singh <asing80@uic.edu>
2019-10-26 23:22:54 -0500

    Update inspection script

--- src/main/resources/bin/inspectdblp.sh
+++ src/main/resources/bin/inspectdblp.sh
@@ -1,11 +1,41 @@
 #!/bin/bash
 
-grep -o '</article>\|</inproceedings>\|</proceedings>\|</book>\|</incollection>\|</phdthesis>\|</mastersthesis>\|</www>' ../dblp.xml | wc -l
+dblp_xml_filepath=$1
+
+grep -o '</article>\|</inproceedings>\|</proceedings>\|</book>\|</incollection>\|</phdthesis>\|</mastersthesis>\|</www>|</data>' "$dblp_xml_filepath" | wc -l
 
 
-grep -o '</author>' dblp.xml | wc -l # use multi
-grep -o '</journal>' dblp.xml | wc -l
-grep -o '</school>' dblp.xml | wc -l # use multi
-grep -o '</year>' dblp.xml | wc -l
-grep -o '</crossref>' dblp.xml| wc -l
-grep -o '</ee>' dblp.xml| wc -l #use multi
+grep -o '</article>' "$dblp_xml_filepath" | wc -l
+grep -o '</inproceedings>' "$dblp_xml_filepath" | wc -l
+grep -o '</proceedings>' "$dblp_xml_filepath" | wc -l
+grep -o '</book>' "$dblp_xml_filepath" | wc -l
+grep -o '</incollection>' "$dblp_xml_filepath"| wc -l
+grep -o '</phdthesis>' "$dblp_xml_filepath"| wc -l
+grep -o '</mastersthesis>' "$dblp_xml_filepath"| wc -l
+grep -o '</www>' "$dblp_xml_filepath"| wc -l
+grep -o '</person>' "$dblp_xml_filepath"| wc -l
+grep -o '</data>' "$dblp_xml_filepath"| wc -l
+
+
+pcregrep -Mi "</author>([\s].*)?<author>" "$dblp_xml_filepath" | wc -l
+pcregrep -Mi "</editor>([\s].*)?<editor>" "$dblp_xml_filepath" | wc -l
+pcregrep -Mi "</title>([\s].*)?<title>" "$dblp_xml_filepath" | wc -l
+pcregrep -Mi "</booktitle>([\s].*)?<booktitle>" "$dblp_xml_filepath" | wc -l
+pcregrep -Mi "</pages>([\s].*)?<pages>" "$dblp_xml_filepath" | wc -l
+pcregrep -Mi "</year>([\s].*)?<year>" "$dblp_xml_filepath" | wc -l
+pcregrep -Mi "</address>([\s].*)?<address>" "$dblp_xml_filepath" | wc -l
+pcregrep -Mi "</journal>([\s].*)?<journal>" "$dblp_xml_filepath" | wc -l
+pcregrep -Mi "</volume>([\s].*)?<volume>" "$dblp_xml_filepath" | wc -l
+pcregrep -Mi "</number>([\s].*)?<number>" "$dblp_xml_filepath" | wc -l
+pcregrep -Mi "</month>([\s].*)?<month>" "$dblp_xml_filepath" | wc -l
+pcregrep -Mi "</url>([\s].*)?<url>" "$dblp_xml_filepath" | wc -l
+pcregrep -Mi "</ee>([\s].*)?<ee>" "$dblp_xml_filepath" | wc -l
+pcregrep -Mi "</cdrom>([\s].*)?<cdrom>" "$dblp_xml_filepath" | wc -l
+pcregrep -Mi "</cite>([\s].*)?<cite>" "$dblp_xml_filepath" | wc -l
+pcregrep -Mi "</publisher>([\s].*)?<publisher>" "$dblp_xml_filepath" | wc -l
+pcregrep -Mi "</crossref>([\s].*)?<crossref>" "$dblp_xml_filepath" | wc -l
+pcregrep -Mi "</isbn>([\s].*)?<isbn>" "$dblp_xml_filepath" | wc -l
+pcregrep -Mi "</series>([\s].*)?<series>" "$dblp_xml_filepath" | wc -l
+pcregrep -Mi "</school>([\s].*)?<school>" "$dblp_xml_filepath" | wc -l
+pcregrep -Mi "</chapter>([\s].*)?<chapter>" "$dblp_xml_filepath" | wc -l
+pcregrep -Mi "</publnr>([\s].*)?<publnr>" "$dblp_xml_filepath" | wc -l
