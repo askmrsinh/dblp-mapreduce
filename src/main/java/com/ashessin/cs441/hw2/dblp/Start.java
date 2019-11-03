00f5792e0a239de86daaa40f59bd20160f7b3abd
Ashesh Kumar Singh <asing80@uic.edu>
2019-11-03 17:44:46 -0600

    Update main Class

--- src/main/java/com/ashessin/cs441/hw2/dblp/Start.java
+++ src/main/java/com/ashessin/cs441/hw2/dblp/Start.java
@@ -12,6 +12,8 @@ import org.apache.commons.cli.Options;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.apache.log4j.PropertyConfigurator.configure;
+
 public class Start {
     private static final Logger logger = LoggerFactory.getLogger(Start.class);
     private Options options;
@@ -19,56 +21,44 @@ public class Start {
 
 
     public static void main(String[] args) throws Exception {
+        System.out.println(Thread.currentThread().getContextClassLoader().getResource("log4j.properties"));
+        configure(Thread.currentThread().getContextClassLoader().getResource("log4j.properties"));
+        org.apache.log4j.Logger.getRootLogger().setLevel(org.apache.log4j.Level.DEBUG);
 
         PublicationsSequenceFileWriter.main(new String[]{
-                "dblp-tiny.xml",
+                args[0],
                 "/cs441/hw2/input/publications.seqfile.deflate"
         });
-
-        System.out.println("\n\n");
-
         PublicationsSequenceFileReader.main(new String[]{
-                "/cs441/hw2/input/publications.seqfile.deflate"
-        });
-
-        System.out.println("\n\n");
+                "/cs441/hw2/input/publications.seqfile.deflate"});
 
         JoinedFieldsCount.main(new String[]{
                 "/cs441/hw2/input/publications.seqfile.deflate",
                 "/cs441/hw2/JoinedFieldsCount",
-                "authors,authors"
-        });
-
-        System.out.println("\n\n");
-
+                "authors,authors"});
         SwapSortKeyValuePairs.main(new String[]{
                 "/cs441/hw2/JoinedFieldsCount/part-r-00000",
-                "/cs441/hw2/SwapSortKeyValuePairs1"
-        });
+                "/cs441/hw2/SwapSortKeyValuePairs1"});
         CopyHdfsFileToLocal.main(new String[]{
-                "/cs441/hw2/SwapSortKeyValuePairs1/part-r-00000.gz"
-        });
-
-        System.out.println("\n\n");
+                "/cs441/hw2/SwapSortKeyValuePairs1/part-r-00000.gz",
+                "SwapSortKeyValuePairs1/part-r-00000.gz"});
+        ExtractLocalGzipFile.main(new String[]{
+                "SwapSortKeyValuePairs1/part-r-00000.gz",
+                args[1]});
 
         SingleFieldCount.main(new String[]{
                 "/cs441/hw2/input/publications.seqfile.deflate",
                 "/cs441/hw2/SingleFieldCount",
-                "authors"
-        });
-
-        System.out.println("\n\n");
-
+                "authors"});
         SwapSortKeyValuePairs.main(new String[]{
                 "/cs441/hw2/SingleFieldCount/part-r-00000",
-                "/cs441/hw2/SwapSortKeyValuePairs2"
-        });
+                "/cs441/hw2/SwapSortKeyValuePairs2"});
         CopyHdfsFileToLocal.main(new String[]{
-                "/cs441/hw2/SwapSortKeyValuePairs2/part-r-00000.gz"
-        });
-
-        System.out.println("\n\n");
-        ExtractLocalGzipFile.main(new String[]{"part-r-00000.gz"});
+                "/cs441/hw2/SwapSortKeyValuePairs2/part-r-00000.gz",
+                "SwapSortKeyValuePairs2/part-r-00000.gz"});
+        ExtractLocalGzipFile.main(new String[]{
+                "SwapSortKeyValuePairs2/part-r-00000.gz",
+                "SwapSortKeyValuePairs2/part-r-00000"});
 
     }
 }
